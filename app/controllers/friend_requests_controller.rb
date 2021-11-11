0b89ffb1fe1834d7b32c6f5bafe04a66fc3954ff
jeff <jmorton138@gmail.com>
2021-11-11 14:47:20 +0000

    Friendrequest button creates friend request in db

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -3,4 +3,19 @@ class FriendRequestsController < ApplicationController
     def index
         # @requests = FriendRequest.where(recipient: current_user)
     end
+
+    def create
+        @friend_request = FriendRequest.new(sender_id: current_user.id, recipient_id: params[:recipient_id])
+
+        if @friend_request.save
+            redirect_to root_path
+        end
+
+    end
+
+    private
+
+    def friend_request_params
+        params.require(:request).permit(:sender_id, :recipient_id)
+    end
 end
