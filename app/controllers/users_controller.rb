9cdc9f1abd3bfdfd9b4916a5325d83d0a6fd65bc
jeff <jmorton138@gmail.com>
2021-11-15 15:40:59 +0000

    Users#index shows all users not already friends or having pending requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,21 @@
 class UsersController < ApplicationController
+    
     def index
-        @users = User.all
-        @request = FriendRequest.new
-        @requests = FriendRequest.where(recipient_id: current_user.id, pending: true)
+
+        @friends = current_user.followers
+       
+        #@requests = FriendRequest.where(sender_id: current_user.id, pending: true)
+        @requests = current_user.sent_requests.where(pending: true)
+        @users = User.all.map do |user|
+            user unless current_user.followers.include?(user) || @requests.where(recipient_id: user.id).exists? || user == current_user 
+        end
+        @users= @users.compact
+        # @request = FriendRequest.new
     end
 
     def show
         @user = current_user
+        @posts = Post.where(author_id: params[:id])
         @requests = FriendRequest.where(recipient_id: current_user.id, pending: true)
 
     end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,6 @@ Rails.application.routes.draw do
 
   resources :users
 
-  devise_for :users
+  devise_for :users,:path => 'u'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
