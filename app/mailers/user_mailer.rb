bdc154e740bed3ce7f7750a32596782db69ef48e
jeff <jmorton138@gmail.com>
2021-11-18 09:36:30 +0000

    Minor edits to welcome mailers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-    default from: 'notifications@example.com'
+    default from: 'fakeymcfake@faker.com'
 
     def welcome_email
       @user = params[:user]
-      @url  = 'http://example.com/login'
+      @url  = 'http://127.0.0.1:3000/'
       mail(to: @user.email, subject: 'Welcome to My Facebook Clone Project')
     end
 end
